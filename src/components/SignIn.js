d382dbefe82f0a3d1f539891cae855879e00eb66
Aniks68 <ikpaulanene@gmail.com>
2022-09-26 23:14:11 +0100

    saved signin with just name

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+import TextField from "@material-ui/core/TextField";
+import React from "react";
+import { Button } from "@material-ui/core";
+import { Controller, useForm } from "react-hook-form";
+import styles from "./SignIn.module.css";
 
 const SignIn = () => {
+    const defaultValues = { username: ""};
+    const { handleSubmit, reset, control, formState: { errors } } = useForm({ defaultValues: defaultValues });
+  const onSubmit = (data) => console.log(data);
   return (
-    <div>SignIn</div>
-  )
+    <form className={styles.form}>
+      <Controller
+        name={"username"}
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <TextField className={styles.input} onChange={onChange} value={value} label={"Username"} required={true}/>
+        )}
+      />
+      {errors.username && <p className={styles.error}>This field is required</p>}
+
+        <Button variant={"contained"} color={"primary"} style={{marginRight: '5px',}} onClick={handleSubmit(onSubmit)}>
+            Sign In
+        </Button>
+        <Button variant={"contained"} style={{color: 'grey'}} onClick={() => reset()}>Reset</Button>
+        
+    </form>
+  );
 }
 
 export default SignIn;
