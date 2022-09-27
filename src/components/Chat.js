7412554f5609038b03504797154cfd419fedbe1f
Aniks68 <ikpaulanene@gmail.com>
2022-09-27 08:15:30 +0100

    displaying time

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -17,13 +17,32 @@ const Chat = () => {
 				setMessages(snapshot.docs.map((doc) => doc.data()));
 			});
 	}, []);
+
+    const formatDateTime = (date) => {
+        const d = date.toDate();
+        // console.log(typeof d);
+        const day = d.getDate();
+        const month = d.getMonth() + 1;
+        const year = d.getFullYear();
+        let hour = d.getHours();
+
+        hour = hour < 10 ? '0' + hour : hour;
+        let minute = d.getMinutes();
+        minute = minute < 10 ? '0' + minute : minute;
+        let second = d.getSeconds();
+        second = second < 10 ? '0' + second : second;
+        let time = (hour > 12) ? (hour-12 + ':' + minute + ':PM') : (hour + ':' + minute +':AM');
+
+        return `${day}/${month}/${year} ${time}`;
+    }
+
 	return (
 		<>
             <div className="chat">
                 <h1>Chat</h1>
                 </div>
 			<div className="msgs">
-				{messages.map(({ id, text, user }) => (
+				{messages.map(({ id, text, user, createdAt }) => (
 					<div>
 						<div
 							key={id}
@@ -31,6 +50,7 @@ const Chat = () => {
 						>
 							<h3 className="user">{user}</h3>
 							<p>{text}</p>
+                            <h3 className="time">{formatDateTime(createdAt)}</h3>
 						</div>
 					</div>
 				))}
