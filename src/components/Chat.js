c853a19ac4128bfd918f0142ecc695a59241d8e8
Aniks68 <ikpaulanene@gmail.com>
2022-09-27 01:01:49 +0100

    handling styles

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,27 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import { db } from '../firebase';
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase";
+import SendMessage from "./SendMessage";
+import { useParams } from "react-router-dom";
 
 const Chat = () => {
-    const [messages, setMessages] = useState([]);
+	const { user } = useParams();
+	const currUser = user;
+	const [messages, setMessages] = useState([]);
 
-    useEffect(() => {
-        db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
-            console.log(snapshot.docs);
-            setMessages(snapshot.docs.map(doc => doc.data()))
-            console.log(messages);
-        })
-    }, []);
-  return (
-    <>
-        <div>Chat</div>
-        {messages.map(({id, text, user, uid}) => (
-            <div key={id}>
-                <h2>{user}</h2>
-                <p>{text}</p>
-            </div>
-        ))}
-    </>
-  )
-}
+	useEffect(() => {
+		db.collection("messages")
+			.orderBy("createdAt")
+			.limit(50)
+			.onSnapshot((snapshot) => {
+				setMessages(snapshot.docs.map((doc) => doc.data()));
+			});
+	}, []);
+	return (
+		<>
+			<div>Chat</div>
+			<div className="msgs">
+				{messages.map(({ id, text, user }) => (
+					<div>
+						<div
+							key={id}
+							className={`msg ${user === currUser ? "sent" : "received"}`}
+						>
+							<h2>{user}</h2>
+							<p>{text}</p>
+						</div>
+					</div>
+				))}
+			</div>
+			<SendMessage />
+		</>
+	);
+};
 
 export default Chat;
--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { db } from '../firebase';
+import { Input } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { useParams } from 'react-router-dom';
 
 const SendMessage = () => {
+    const [message, setMessage] = useState('');
+    const { user } = useParams();
+    const sendMessage = async (e) => {
+        e.preventDefault();
+        await db.collection('messages').add({
+            text: message,
+            createdAt: new Date(),
+            user: user,
+        });
+        setMessage('');
+    };
   return (
-    <div>SendMessage</div>
-  )
+    <>
+    <form>
+        <Input value={message} onChange={(e) => setMessage(e.target.value)} placeholder='Message...'/>
+        <Button type='submit' onClick={sendMessage}> Send </Button>
+    </form>
+    </>
+    )
 }
 
 export default SendMessage;
